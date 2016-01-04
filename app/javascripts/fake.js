2395a5f933bf444d2f8915ec08fde9d13a6c0b67
stephjiayi <steph.jiayi@gmail.com>
2016-01-04 17:50:15 +0100

    Mock Socket.IO

--- app/javascripts/fake.js
+++ app/javascripts/fake.js
@@ -1,40 +1,62 @@
 'use strict';
 
+/**
+ * Returns fake list of people from server
+ * @return {array}
+ */
 var getPeopleList = function() {
   return [
     {
       name: 'Daenerys',
-      _id: '1',
+      _id: 'id_1',
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg'
     },
     {
       name: 'Jon',
-      _id: '2',
+      _id: 'id_2',
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/teclaro/128.jpg'
     },
     {
       name: 'Sansa',
-      _id: '3',
+      _id: 'id_3',
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
     },
     {
       name: 'Arya',
-      _id: '4',
+      _id: 'id_4',
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/nuraika/128.jpg'
     },
     {
       name: 'Cersei',
-      _id: '5',
+      _id: 'id_5',
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg'
     },
     {
       name: 'Joffrey',
-      _id: '6',
+      _id: 'id_6',
       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/jfkingsley/128.jpg'
     }
   ];
 };
 
+/**
+ * Fake Socket.IO API
+ */
+var callbackMap = {};
+var mockSio = {
+  on: function(msgType, callback) {
+    callbackMap[msgType] = callback;
+  },
+  emit: function(msgType, data) {
+    if(msgType === 'login' && callbackMap.login) {
+      setTimeout(function() {
+        callbackMap.login(data);
+      }, 1000);
+    }
+  }
+};
+
 module.exports = {
-  getPeopleList: getPeopleList
+  getPeopleList: getPeopleList,
+  mockSio: mockSio
 };
