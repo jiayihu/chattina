53fd7460c0e84857b58b33652e486e397c12307d
stephjiayi <steph.jiayi@gmail.com>
2016-01-22 20:11:58 +0100

    remove docs

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,7 +6,7 @@ var shell = require('./shell');
 
 var init = function() {
   helpers.init();
-  // helpers.makeError('The app is still in production, not ready for usage. Please have a cookie and come back again later.');
+  helpers.makeError('The app is still in production, not ready for usage. Please have a cookie and come back again later.');
 
   model.init();
   shell.init();
