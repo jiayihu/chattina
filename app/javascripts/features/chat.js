9dbc611ad1f8f1d4d6f8e53620b68d19ab5a19f0
stephjiayi <steph.jiayi@gmail.com>
2016-01-22 19:12:38 +0100

    People list UI and toggle button

---
+++ app/javascripts/features/chat.js
@@ -0,0 +1,25 @@
+/**
+ * Chat module
+ * @module features/chat
+ */
+
+'use strict';
+
+var configMap = {
+
+};
+
+
+
+/**
+ * PUBLIC FUNCTIONS
+ */
+
+var init = function() {
+
+};
+
+module.exports = {
+  /** Init the chat module */
+  init: init
+};
